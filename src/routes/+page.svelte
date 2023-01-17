7abdf7d17d77298cf794bebe844b53ae540d8c7e
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2023-01-17 22:17:10 +0300

    Final v0.1
    :partying_face:

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<div class="min-h-screen flex flex-col  justify-center space-y-3  items-center bg-black/90">
+    <h1 class="font-inter font-bold text-white text-5xl">
+        <span class="italic text-white/10">Hello World.</span> Hello World. <span class="font-mono text-white/10">Hello World.</span>
+
+    </h1>
+    <div class="flex space-x-2">
+        <img class="h-10 rounded-full" src="https://images-ext-1.discordapp.net/external/frxGvdnRK5Vhp429RlKtU4snjQRzJzAsq3Y9pf3Z6f8/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/438037153310834700/c34913fa7540ecbe91775179d8ed90f9.png" alt="pp">
+        <a href="https://github.com/xDarkShiny/svelte-tailwind-starter" class="py-2 px-5 bg-white rounded-full font-inter font-light">Github</a>
+    </div>
+    <div class="flex flex-col drop-shadow-2xl">
+        <div class="flex w-[30rem] h-10 border-b-2  text-center px-5 items-center border-white/20 bg-black rounded-t-lg">
+            <div class="space-x-2 flex">
+                <div class="flex h-3 w-3 bg-red-500 rounded-full"></div>
+                <div class="flex h-3 w-3 bg-yellow-500 rounded-full"></div>
+                <div class="flex h-3 w-3 bg-green-500 rounded-full"></div>
+            </div>            
+            <h1 class="font-inter text-white justify-center ml-20">svelte-tailwind-starter</h1>
+        </div>
+        <div class="flex flex-col  py-5 px-5  rounded-b-lg w-[30rem] h-52 bg-black">
+            <h1 class="text-white font-mono"><span class="text-gray-500">$</span> <span class="text-yellow-500">git</span> clone https://github.com/xDarkShiny/svelte-tailwind-starter</h1>
+            <h1 class="text-white font-mono"><span class="text-gray-500">$</span> <span class="text-yellow-500">npm</span> install</h1>
+            <h1 class="text-white font-mono"><span class="text-gray-500">$</span> <span class="text-yellow-500">npm</span> run dev <span class="text-gray-500">-- --open</span></h1>
+            <h1 class=" ml-20 mt-3 font-mono  text-white">                
+                <span class="text-green-500">VITE v4.0.4</span>  <span class="text-gray-500">ready in </span> 1881 ms
+            </h1>
+            <h1 class="ml-20 mt-3 font-mono  text-white">                
+                <span class="text-green-500">➜</span>  Local:   <span class="text-blue-500">http://localhost:1453/</span> 
+            </h1>
+        </div>
+        <div class="flex space-x-2 text-center ml-3">
+            <h1 class="font-inter text-white/50 ">tailwind v3.2.4</h1>
+            <h1 class="font-inter text-white/50 ">sveltekit v1.0.0</h1>
+        </div>
+    </div>
+</div>
