70b3cebeeddd0c740857a16beb1245c02096e9bf
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2023-01-17 21:28:31 +0300

    deneme

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="min-h-screen flex flex-col justify-center space-y-3  items-center bg-black/90">
+    <h1 class="font-inter font-bold text-white text-5xl">
+        Hello World.
+    </h1>
+    <div class="flex space-x-2">
+        <img class="h-10 rounded-full" src="https://images-ext-1.discordapp.net/external/frxGvdnRK5Vhp429RlKtU4snjQRzJzAsq3Y9pf3Z6f8/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/438037153310834700/c34913fa7540ecbe91775179d8ed90f9.png" alt="pp">
+        <a href="githubamk" class="py-2 px-5 bg-white rounded-full font-inter font-semibold">Github</a>
+    </div>
+</div>
